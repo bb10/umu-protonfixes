98e3126511930ea6dbb06fee77fb85dc0c14b6de
bb10 <607398+bb10@users.noreply.github.com>
2025-02-19 05:46:15 +0100

    Add save detection fix for Atelier Ryza 3

--- gamefixes-steam/1999770.py
+++ gamefixes-steam/1999770.py
@@ -8,3 +8,12 @@ from protonfixes import util
 
 def main() -> None:
     util.disable_protonmediaconverter()
+    
+    """Grants bonus content to players with save data for Atelier Ryza: Ever Darkness & the Secret Hideout"""
+    util.import_saves_folder(1121560, 'Documents/KoeiTecmo/Atelier Ryza')
+
+    """Grants bonus content to players with save data for Atelier Ryza 2: Lost Legends & the Secret Fairy"""
+    util.import_saves_folder(1257290, 'Documents/KoeiTecmo/Atelier Ryza 2')
+
+    """Grants bonus content to players with save data for Atelier Sophie 2: The Alchemist of the Mysterious Dream"""
+    util.import_saves_folder(1621310, 'Documents/KoeiTecmo/Atelier Sophie 2')

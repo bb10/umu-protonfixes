e2f8a0396ab7d34cfebc4bda4e2279fc210255f5
bb10 <607398+bb10@users.noreply.github.com>
2025-02-19 05:59:57 +0100

    Add save detection for BLUE REFLECTION: Second Light

---
+++ gamefixes-steam/1423600.py
@@ -0,0 +1,5 @@
+"""BLUE REFLECTION: Second Light"""
+
+def main() -> None:
+    """Grants bonus content to players with save data for Atelier Sophie 2: The Alchemist of the Mysterious Dream (only if you own pre-order editions of both games)"""
+    util.import_saves_folder(1621310, 'Documents/KoeiTecmo/Atelier Sophie 2')
